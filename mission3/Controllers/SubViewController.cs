9732cfc74f73ca682a997d9ee5784b68a25eb0d9
EzraMarin <90418312+EzraMarin@users.noreply.github.com>
2022-01-24 14:04:58 -0700

    Add project files.

---
+++ mission3/Controllers/SubViewController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mission3.Models;
+
+namespace Mission3.controllers
+{
+    public class SubView : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
+
